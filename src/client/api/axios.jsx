e7b7ca8a03520b1f1d17bcd75b2a4685b7435fba
ludde94 <l.jenisch@hotmail.com>
2023-05-14 19:50:13 +0200

    deployment testing

--- src/client/api/axios.jsx
+++ src/client/api/axios.jsx
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-const baseURL = `https://${window.location.hostname}:10000/api`;
-
+let baseURL = '';
+// Check if running in development or production environment
+if (process.env.NODE_ENV === 'development') {
+  // Development environment
+  baseURL = 'http://localhost:10000/api';
+} else {
+  // Production environment
+  baseURL = `${window.location.origin}/api`;
+}
 export default axios.create({
-    baseURL,
+  baseURL,
 });
